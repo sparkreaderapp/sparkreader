e2dcb52c2533a35c2f9416c33e2062771a6bedf4
sparkreaderapp <220067831+sparkreaderapp@users.noreply.github.com>
2025-11-18 11:30:59 +0000

    refactor: Add JAVA_HOME path handling to build-release-aab.bat script

--- build-release-aab.bat
+++ build-release-aab.bat
@@ -1,6 +1,17 @@
 @echo off
+setlocal enabledelayedexpansion
+
 echo Building signed release AAB...
 echo.
+
+REM Ensure JAVA_HOME/bin is in PATH
+if defined JAVA_HOME (
+    set "PATH=%JAVA_HOME%\bin;%PATH%"
+    echo Added JAVA_HOME\bin to PATH: %JAVA_HOME%\bin
+) else (
+    echo Warning: JAVA_HOME environment variable not set. Setting it to ..\..\apps\android-studio\jbr
+    set JAVA_HOME=..\..\apps\android-studio\jbr
+)
 
 REM Clean the project first
 echo Cleaning project...
